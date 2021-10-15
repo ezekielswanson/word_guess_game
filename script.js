6ccbc0449c0e146cf07061e96c9f9d32c8165184
ezekielswanson <swansonezekiel@gmail.com>
2021-10-14 20:30:05 -0500

    Started working on js file.

---
+++ script.js
@@ -0,0 +1,23 @@
+/* 
+1. Create array of phrases
+
+2. function grab phrase at random
+
+3. function display phrase at random
+
+*/
+
+const randomLetter = getElementById('qwerty');
+const randomPhrase = getElementById('phrase');
+const missed = 0;
+const overlay = getElementById('overlay');
+
+//Creating event listner to hide start game overlay screen
+overlay.addEventListener('click', () => {
+    overlay.style.display = 'none';
+});
+
+
+
+
+});
