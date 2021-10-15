bc45d6816d811df25e20024b0917cfa1c8c7d9f8
ezekielswanson <swansonezekiel@gmail.com>
2021-10-14 20:36:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,17 +7,19 @@
 
 */
 
+//Variables
 const randomLetter = getElementById('qwerty');
 const randomPhrase = getElementById('phrase');
 const missed = 0;
 const overlay = getElementById('overlay');
+//Can I put the overlay var inside of the event listner?
 
 //Creating event listner to hide start game overlay screen
 overlay.addEventListener('click', () => {
     overlay.style.display = 'none';
+    //what do I name my return?
 });
 
 
-
-
-});
+//Phrases array 
+const phrases = ['Apple', 'Orange', 'Kiwi', 'Almond', 'Garlic']
