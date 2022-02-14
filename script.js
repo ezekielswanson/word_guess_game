19b063482dc8f08568607408554261ce36ab4e36
ezekielswanson <75798690+ezekielswanson@users.noreply.github.com>
2022-02-13 18:33:23 -0600

    Updated js script.

--- script.js
+++ script.js
@@ -17,29 +17,17 @@ btnReset.addEventListener('click', ()=> {
 //Phrases array 
 const phrases = ['Apples to Oranges', 'The day is young', "If you are not first you are last", 'Ricky Bobby', "The early bird gets the worm"]
 
-//Creating a function to select an val from array at random
+//Function selects an value from the array at random
 getRandomPhraseAsArray = (arr) => {
     const randomPhraseNumber = Math.floor(Math.random()*arr.length);
     return arr[randomPhraseNumber].split("");
 };
 
-//Create a variable to access the getRandomPhraseAsArray function
-/*The getRandomPhraseAsArray function is already getting the random split 
-character and you're passing the phrases as an arugment(value) into that function
-*/
+//Variable to access the getRandomPhraseAsArray function
 const randomPhrase = getRandomPhraseAsArray(phrases);
-/*Will call this randomphrase as a argument 
-when I call addPhraseToDisplay and use randomPhrase I'm 
-switching out arr for randomPhrase value
-*/
 
 
-
-// Displaying the randmonly selected array phrase
-//display characters on page 
-/*If this person is not zeke do this
-for 2 options
-*/
+// Function displays the randmonly selected array phrase
 const addPhraseToDisplay = (arr) => {
 
         for (let i = 0; i < arr.length; i++) {
@@ -47,9 +35,6 @@ const addPhraseToDisplay = (arr) => {
             const item = document.createElement('li');
 
 
-
-        //Space b/w quatation marks rep a space 
-        //if you want actual word 
         if (arr[i] !== ' ') {
 
             item.classList.add('letter');
@@ -73,8 +58,7 @@ addPhraseToDisplay(randomPhrase);
 
 
 
-// Creating checkLetter function
- 
+//Function checks if user button clicks on a button that matches the random phrase
 function checkLetter(button) {
    const letterClass = document.getElementsByClassName('letter');
    let match = null;
@@ -91,27 +75,18 @@ function checkLetter(button) {
    return match;
 }
 
-/*We're not passing an argument into this function because letterClass 
-using the data from the array*/
 
-
-//Creating event listner 
-//Using e because we want target the button elemenet only 
-
+//Click event/function removes a heart at the bottom of the screen if user selects wong button
 qwerty.addEventListener('click', (e) => {
 
-    //Ref class name and tag name  b/c it's more specific 
     const img = document.querySelectorAll('.tries img');
-    //set var before if statements 
-    //b/c it only runs spec code for the if statements 
 
-    //Using button b/c tagName proper is uppercase
+
     if (e.target.tagName === 'BUTTON' && !e.target.classList.contains("chosen")) {
 
         e.target.classList.add('chosen');
    
    
-     //passing what letter was clicked as an arugment
      const selectedButton = checkLetter(e.target.textContent.toLowerCase());
 
      if (selectedButton) {
@@ -149,6 +124,10 @@ qwerty.addEventListener('click', (e) => {
                  checkWin()
     });
 
+
+/*Function shows win overlay if user guesses phrase right and lose overlay if user
+does guess phrase correct
+*/
 const checkWin = () => {
     const letterClassMatch = document.getElementsByClassName('letter');
     const showClassMatch = document.getElementsByClassName("show");
